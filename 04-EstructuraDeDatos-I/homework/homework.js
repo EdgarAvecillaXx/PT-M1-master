14ac1007ef07fad6eea7efb2de7ff11be3259d74
Edgar Avecilla <edgaravecilla1993@gmail.com>
2022-08-13 10:45:48 -0500

    Homework4 Done!

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*
 Definir las funciones recursivas nFactorial y nFibonacci.
@@ -8,6 +8,8 @@ nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número 
 nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
 Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
 
+return n | 0 y 1 | n-1 y n-2 |
+
 Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
 
 
@@ -15,9 +17,30 @@ Como ejercicio adicional y completamente opcional, al terminar de resolver este 
 */
 
 function nFactorial(n) {
+  parseInt(n);
+  if (n < 0) throw "Error! Factorial for negative numbers does not exist";
+  return n <= 1 ? n : n * nFactorial(n - 1);
 }
 
-function nFibonacci(n) {
+// fn = fn-1 + fn-2;
+/*function nFibonacci(n) {
+  parseInt(n);
+  if (n < 0) throw "Error!";
+  if (n <= 1) return n;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
+}*/
+
+function nFibonacci(n, memo = {}) {
+  parseInt(n);
+  if (n < 0) throw "Error!";
+
+  //memoization
+  if (!memo[n]) {
+    n <= 1
+      ? (memo[n] = n)
+      : (memo[n] = nFibonacci(n - 1, memo) + nFibonacci(n - 2, memo));
+  }
+  return memo[n];
 }
 
 /*
@@ -29,8 +52,50 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
+class Queue {
+  //#elements = {};
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.len = 0;
+  }
+
+  enqueue(data) {
+    const newNode = new Node(data);
+    this.len === 0 ? (this.head = newNode) : (this.tail.next = newNode);
+    this.tail = newNode;
+    this.len++;
+    //this.#elements[this.tail] = data;
+    //this.tail++;
+    //this.len++;
+  }
+
+  dequeue() {
+    if (this.len === 0) {
+      this.tail = null;
+      return undefined;
+    }
+    //const element = this.#elements[this.head];
+    //delete this.#elements[this.head];
+    //this.head++;
+    //this.len--;
+    //return element;
+    const exitNode = this.head;
+    this.head = exitNode.next;
+    this.len--;
+    return exitNode.data;
+  }
+
+  size() {
+    return this.len;
+  }
 }
 
 // No modifiquen nada debajo de esta linea
@@ -39,5 +104,5 @@ function Queue() {
 module.exports = {
   Queue,
   nFactorial,
-  nFibonacci
+  nFibonacci,
 };
